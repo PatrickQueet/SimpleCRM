3ae9c9f60821da921b1006040eb5546435ae7fe1
PatrickQueet <patrickqueet@gmail.com>
2020-10-02 09:27:30 +1200

    Updates

---
+++ precache-manifest.68cb3349c71078044ac35667037aa9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6455a0c894143e829d8cfd9eb14e2d02",
+    "url": "/PatrickQueet/SimpleCRM.git/index.html"
+  },
+  {
+    "revision": "167d92d9caf8e088a856",
+    "url": "/PatrickQueet/SimpleCRM.git/static/css/main.d86d56e4.chunk.css"
+  },
+  {
+    "revision": "a0966f88720a0b821ce9",
+    "url": "/PatrickQueet/SimpleCRM.git/static/js/2.e57c0874.chunk.js"
+  },
+  {
+    "revision": "9da86d9e7b603553ac84ff299852fb98",
+    "url": "/PatrickQueet/SimpleCRM.git/static/js/2.e57c0874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167d92d9caf8e088a856",
+    "url": "/PatrickQueet/SimpleCRM.git/static/js/main.c8f4f140.chunk.js"
+  },
+  {
+    "revision": "2103040216294f3ba4a4",
+    "url": "/PatrickQueet/SimpleCRM.git/static/js/runtime-main.7e41bfcc.js"
+  },
+  {
+    "revision": "ba888030439fec41c74760c920e4775f",
+    "url": "/PatrickQueet/SimpleCRM.git/static/media/Newnet_Fiji_Logo.ba888030.jpg"
+  },
+  {
+    "revision": "01de5612376637bd3d6c9a6db4c27752",
+    "url": "/PatrickQueet/SimpleCRM.git/static/media/shape.01de5612.svg"
+  }
+]);
